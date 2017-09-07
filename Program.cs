0408fc0479fa727d864986c0da2eb9e6578eea6b
Carolyn O Jillson <carolynj70@gmail.com>
2017-09-07 16:05:39 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -30,7 +30,15 @@ namespace StoryBuilding
             Console.WriteLine("Nice to meet you " + name);
             Console.WriteLine("What is your story about?");
             string storySubject = Console.ReadLine();
+            Console.WriteLine("How do you plan on preparing your next meal? Will it be ...");
+            Console.WriteLine("1. Straight from the refridgerator");
+            Console.WriteLine("2. Baked in the oven");
+            Console.WriteLine("3. Cooked on the stovetop");
+            Console.WriteLine("4. It's in the slow cooker");
+            Console.WriteLine("5. Eating out");
+            string mealPrep = Console.ReadLine();
 
+            
             string finalWord = Console.ReadLine();
 
         }
